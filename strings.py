ceed95028ec1a4f34fe88be7b6b171b19e808939
ArerepadeB <aredelulu@gmail.com>
2021-05-22 11:26:24 -0500

    first scripts

---
+++ strings.py
@@ -0,0 +1,25 @@
+print("Today is a good day to learn python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + "world")
+
+# if we want space, we can add that too
+# print (greeting + '' + name)
+
+
+# Variables
+greeting = "Hello"
+# name = "Bruce"
+name = input("Louis Benagha")
+print(greeting + name)
+print(greeting + '' + name)
+
+age = 24
+print(age)
+
+# To know what variable types are?, python interprets them as such
+print(type(age))
+print(type(greeting))
+
+
