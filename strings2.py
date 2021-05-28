372fec5538ec55d6a94b18cfe4aee9d82b378982
ArerepadeB <aredelulu@gmail.com>
2021-05-28 18:30:38 -0500

    codes

--- strings2.py
+++ strings2.py
@@ -47,3 +47,17 @@ print(parrot[:6] + parrot[6:])
 # test
           #01234567890123456789012345#
 letters = "abcdefghijklmnopqrstuvwxyz"
+
+# slice
+
+number = "9,223,372,036,854,775,807"
+print(number[1::4])
+
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
+
+
