52a93f2e522830e91ea4419fcd1dd469288225e9
ArerepadeB <aredelulu@gmail.com>
2021-05-30 17:43:54 -0500

    demo2 lectures

---
+++ test.py
@@ -0,0 +1,14 @@
+quantity = 10
+price = 5.0
+total = quantity * price
+tax = total / 5
+Total = total + tax
+print(total)
+
+days = "Mon, Tue, Wed, Thu, Fri, Sat, Sun"
+print(days[::5])
+print()
+
+flower = "blue violet"
+print('blue' in flower)
+print("blue" in flower)
