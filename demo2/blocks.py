7654e70b6c38bfa923d8747ae290de5d378d2850
ArerepadeB <aredelulu@gmail.com>
2021-05-30 18:58:28 -0500

    conditions in python

--- demo2/blocks.py
+++ demo2/blocks.py
@@ -1,29 +1,30 @@
-# for i in range(1, 13):
-#     print("No. {:0} square is {:2} and cubed is {:4}".format(i, i ** 2, i ** 3))
-#     print("*" * 80)
-# print("*" * 80)
+for i in range(1, 13):
+    print("No. {:0} square is {:2} and cubed is {:4}".format(i, i ** 2, i ** 3))
+    print("*" * 80)
+print("*" * 80)
 
-# name = input("Please enter your name:")
-# age = input("How old are you you, {0}? ".format(name))
-# print(age)
-#
-# print()
-# name = input("Please enter your name:")
-# age = int(input("How old are you you, {0}? ".format(name)))
-# print(age)
-#
-# print()
-#
+name = input("Please enter your name:")
+career = input("Cloud engineer")
+age = input("How old are you you, {0}? ".format(name, career))
+print(age, career)
+
+print()
 name = input("Please enter your name:")
 age = int(input("How old are you you, {0}? ".format(name)))
 print(age)
-# # Conditions
-# if age >= 18:
-#     print("""You are old enough to vote""")
-#     print("""Please put an X in the box""")
-# # else clause
-# else:
-#     print("Please come back in {0} years".format(18 - age))
+
+print()
+
+name = input("Please enter your name:")
+age = int(input("How old are you you, {0}? ".format(name)))
+print(age)
+# Conditions
+if age >= 18:
+    print("""You are old enough to vote""")
+    print("""Please put an X in the box""")
+# else clause
+else:
+    print("Please come back in {0} years".format(18 - age))
 
 if age < 18:
     print("Please come back in {0} years".format(18 - age))
