1bbb865cd83abacead46fd7091ee95701d315484
ArerepadeB <aredelulu@gmail.com>
2021-06-13 08:30:39 -0500

    if, else statements

---
+++ demo2/exercise.py
@@ -0,0 +1,23 @@
+tree1 = "Oak Tree"
+tree2 = "Palm Tree"
+# add the code to compare the trees
+if tree1 == tree2:
+ print("The trees are the same")
+else:
+ print("The trees are different")
+
+print()
+gameA = "playstation"
+gameB = "playstation"
+if gameA == gameB:
+ print("what a lovely game to have")
+else:
+ print("Save money to buy yourself a Playstation it is better")
+
+print()
+gameA = "playstation"
+gameB = "xbox"
+if gameA == gameB:
+ print("what a lovely game to have")
+else:
+ print("Save money to buy yourself a Playstation it is better")
