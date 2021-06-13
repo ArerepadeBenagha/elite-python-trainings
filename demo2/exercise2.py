6e9d1de42a9b3350a30cd250363042a01c053c73
ArerepadeB <aredelulu@gmail.com>
2021-06-13 08:43:52 -0500

    if, else statements

---
+++ demo2/exercise2.py
@@ -0,0 +1,18 @@
+x = 5
+y = 7
+
+if x > y:
+    print("that answer is incorrect")
+else:
+    print("that answer is correct")
+
+x = 5
+y = 7
+
+print()
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is smaller than y")
+else:
+    print("x equals y")
