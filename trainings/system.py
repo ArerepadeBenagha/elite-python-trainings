be83f45d0e61b9fd491523afccc37a316d32ef0a
Arerepade Benagha <aredelulu@gmail.com>
2022-06-05 01:47:11 -0500

    update

--- trainings/system.py
+++ trainings/system.py
@@ -1,21 +1,3 @@
-# import os
-# print(os.getcwd())
-
-# Execute linux command with system
-
-# print(os.system("ls -l"))
-# print(os.system("pwd"))
-# print(os.system("touch random.txt"))
-#
-# cmd = "date"
-# x = os.system(cmd)
-#
-# if x == 0:
-#     print("your command ran successfully")
-# else:
-#     print("your command failed")
-
-# script to clear screen for both windows and linux machine #
 import os
 import platform
 
